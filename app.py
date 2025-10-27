b2bf1bdc3bac33d82f0abecd050ce4d080dbb4b7
grace_scale <alexandrsudin@gmail.com>
2025-10-27 17:05:57 +0100

    Add books

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 import os
@@ -11,8 +11,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 db.init_app(app)
 
-with app.app_context():
-  db.create_all()
+"""with app.app_context():
+  db.create_all()"""
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/add_author', methods=['GET', 'POST'])
@@ -28,8 +33,25 @@ def handle_author():
         )
         db.session.add(author)
         db.session.commit()
-        return render_template('home.html')
+        return redirect(url_for('home'))
     return render_template('add_author.html')
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def handle_book():
+    if request.method == 'POST':
+        book = Book(
+            title=request.form["title"],
+            author_id=request.form["author_id"],
+            isbn=request.form["isbn"],
+            publication_year=request.form["publication_year"]
+        )
+        db.session.add(book)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    authors = Author.query.all()
+    return render_template('add_book.html', authors=authors)
 
 
 if __name__ == '__main__':
